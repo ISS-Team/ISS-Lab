9018b6fda4ac6a57a1680ce6cde6e6e64e7b28f3
afterwind <afterwinddesu@gmail.com>
2017-04-26 18:14:26 +0300

    Added server files

---
+++ server/src/main/java/backend/Main.java
@@ -0,0 +1,12 @@
+package backend;
+
+/**
+ * Main class for starting the backend
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("This is backend");
+    }
+
+}
---
+++ server/src/main/java/middleware/Main.java
@@ -0,0 +1,12 @@
+package middleware;
+
+/**
+ * Main class for starting the middleware
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("This is middleware");
+    }
+
+}
