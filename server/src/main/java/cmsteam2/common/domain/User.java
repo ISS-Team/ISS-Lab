14fcde87c41e4f66f90ad5218669bb46d6e6fc6b
AfterWind <sergius302@gmail.com>
2017-05-10 21:41:13 +0300

    Added permissions to User class.

--- server/src/main/java/cmsteam2/common/domain/User.java
+++ server/src/main/java/cmsteam2/common/domain/User.java
@@ -7,11 +7,37 @@ public class User {
     public final String email;
     public final boolean isActivated;
 
-    public User(String name, String password, String email, boolean isActivated) {
+    /**
+     * Permission level. The higher the more permission the user has.
+     */
+    private int permissionLevel;
+
+    public User(String name, String password, String email, boolean isActivated, Permission permission) {
         this.name = name;
         this.password = password;
         this.email = email;
         this.isActivated = isActivated;
+        this.permissionLevel = permission.level;
     }
 
+    public boolean hasPermission(Permission perm) {
+        return perm.level <= permissionLevel;
+    }
+
+    public void setPermissionLevel(Permission perm) {
+        this.permissionLevel = perm.level;
+    }
+
+    public enum Permission {
+        LISTENER(0),
+        AUTHOR(1),
+        MEMBER(2),
+        CHAIR(3);
+
+        private int level;
+
+        Permission(int level) {
+            this.level = level;
+        }
+    }
 }
