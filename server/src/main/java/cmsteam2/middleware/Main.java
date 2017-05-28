ffedeaee3f35c6d7a360cc90354f863b9dc7aadb
Bogdan Oniga <bogdan.oniga13@gmail.com>
2017-05-28 11:03:47 +0300

    UserController update

--- server/src/main/java/cmsteam2/middleware/Main.java
+++ server/src/main/java/cmsteam2/middleware/Main.java
@@ -1,5 +1,9 @@
 package cmsteam2.middleware;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -11,10 +15,28 @@ import org.springframework.context.annotation.ComponentScan;
 @ComponentScan(basePackages = {"cmsteam2.rest.controller"})
 public class Main {
 
+    public static SessionFactory sessionFactory;
+
     public static void main(String[] args) {
 //        System.out.println("This is middleware");
+        initialize();
         SpringApplication.run(Main.class, args);
+    }
 
+    static void initialize(){
+        final StandardServiceRegistry registry=new StandardServiceRegistryBuilder().configure().build();
+        try {
+            sessionFactory =new MetadataSources(registry).buildMetadata().buildSessionFactory();
+        }catch (Exception e){
+            StandardServiceRegistryBuilder.destroy(registry);
+            e.printStackTrace();
+        }
+    }
+
+    static void close(){
+        if(sessionFactory !=null){
+            sessionFactory.close();
+        }
     }
 
 }
