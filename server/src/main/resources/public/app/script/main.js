2d63d902e76c52a179525b32bf19ac5b53f5fa82
AfterWind <sergius302@gmail.com>
2017-06-01 22:19:01 +0300

    "Merged" front-end files.

--- server/src/main/resources/public/app/script/main.js
+++ server/src/main/resources/public/app/script/main.js
@@ -3,6 +3,44 @@
  */
 $(document).ready(function () {
     console.log("ready");
+
+    $("tr").click(function() {
+
+        <!-- var table, rows, switching, i, x, y, shouldswitch,vector1={},vector2={}; -->
+        <!-- table=$("#tablegeneralinformations")[0];  -->
+        <!-- rows=$("tr"); -->
+        <!-- for (i = 1; i < (rows.length)-1; i++) { -->
+        <!-- x=rows[i].getelementsbytagname("td")[1].innerhtml; -->
+        <!-- alert(x); -->
+        <!-- } -->
+        var id_conference = $(this).find("*:first-child").html();
+        $.ajax({
+            url: "/conferences/" + id_conference + "/sessions/getall",
+            type: "GET",
+            //contentType: "application/json",
+            // dataType: "json",
+            // data: JSON.stringify(id_conference),
+            success: function (data) {
+                alert("Success");
+                console.log(data);
+                //$each(res.id,res.title,res.theme,res.date,res.deadLineAbstractInfo,res.deadLineFullPaper,res.deadLineReview,res.startTime,res.endTime){
+                var tr;
+                for (var i = 0; i < data.length; i++) {
+                    tr = $('<tr/>');
+                    //tr.append("<td style='display:none'>" + data[i].id+ "</td>");
+                    tr.append("<td>" + data[i].title + "</td>");
+                    tr.append("<td>" + data[i].startTime + "</td>");
+                    tr.append("<td>" + data[i].duration + "</td>");
+                    $("#sessionInfo").append(tr);
+                }
+            },
+            error: function (res) {
+                alert("Eroare");
+            }
+
+
+        });
+    });
     $("#Login").click(function () {
         $("#generalInformations").hide();
         $("#formRegister").hide();
@@ -19,6 +57,37 @@ $(document).ready(function () {
         $("#formLogin").hide();
         $("#formRegister").hide();
         $("#generalInformations").show();
+
+        $.ajax({
+
+            url: "/users/register",
+            type: "GET",
+            //   contentType: "application/json",
+            //  dataType: "json",
+            // data: JSON.stringify(forRegister),
+            success: function (data) {
+                alert("Success");
+                console.log(data);
+                //$each(res.id,res.title,res.theme,res.date,res.deadLineAbstractInfo,res.deadLineFullPaper,res.deadLineReview,res.startTime,res.endTime){
+                var tr;
+                for (var i = 0; i < data.length; i++) {
+                    tr = $('<tr/>');
+                    tr.append("<td style='display:none' id='idConference'>" + data[i].id + "</td>");
+                    tr.append("<td>" + data[i].title + "</td>");
+                    tr.append("<td>" + data[i].theme + "</td>");
+                    tr.append("<td>" + data[i].date + "</td>");
+                    tr.append("<td>" + data[i].deadLineAbstractInfo + "</td>");
+                    tr.append("<td>" + data[i].deadLineFullPaper + "</td>");
+                    tr.append("<td>" + data[i].deadLineReview + "</td>");
+                    tr.append("<td>" + data[i].startTime + "</td>");
+                    tr.append("<td>" + data[i].endTime + "</td>");
+                    $("#tableGeneralInformations").append(tr);
+                }
+            },
+            error: function (res) {
+                alert("Eroare");
+            }
+        });
     });
 
     $("#btnSubmitRegister").click(function () {
